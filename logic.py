3103c4cca4748348143a345522240272ee2f9738
alexyungkk <w0517150@nscc.ca>
2026-05-28 12:29:41 -0300

    Added update function, remove item function, and prevent empty input

--- logic.py
+++ logic.py
@@ -1,9 +1,9 @@
 import json
+import streamlit as st
 import os
 
 floors = ["7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"]
-
-quantity = ["Below 20%", "20%","40%","60%","80%","100%"]
+quantity = [0, 20, 40, 60, 80, 100]
 
 def initialize_json():
     initial_data = {
@@ -49,21 +49,51 @@ def initialize_json():
     }
     return initial_data
 
-
-
-def add_item(newItem, items, filename="stock.json"):
+def add_item(newItem, filename="stock.json"):
     with open(filename, "r+") as file:
         file_data = json.load(file)
-        for i in file_data:
-            file_data[i]["inventory"].update(
-                {f"{newItem}": {
-                    "Update Time": "",
-                    "Quantity": ""
+        if not newItem.strip():
+            st.error("Please input an item")           
+        else:
+            for i in file_data:
+                file_data[i]["inventory"].update(
+                    {f"{newItem}": {
+                        "Update Time": "",
+                        "Stock Level": ""
+                        }
                     }
-                }
-            )
+                )
+            st.success(f"{newItem} was added sucessfully.")
+            file.seek(0)
+            file.truncate()
+            json.dump(file_data, file, indent=4)
+    
+def remove_item(removeItem, filename="stock.json"):
+    with open(filename, "r+") as file:
+        file_data = json.load(file)
+        if not removeItem.strip():
+            st.error("Please input an item")  
+        elif removeItem in file_data["7"]["inventory"]:
+            for i in file_data:
+                file_data[i]["inventory"].pop(removeItem, None)
+            st.success(f"{removeItem} was deleted sucessfully.")
+        else:
+            st.error(f"There is no {removeItem}. ")
         file.seek(0)
         file.truncate()
-        json.dump(file_data, file, indent=4)
+        json.dump(file_data, file, indent=4 )
 
-
+def update_stock(floor, item, stockLevel, time, filename="stock.json"):
+    with open(filename, "r+") as file:
+        file_data = json.load(file)
+        file_data[floor]["inventory"].update(
+            {f"{item}": {
+                "Update Time": f"{time}",
+                "Stock Level": f"{stockLevel}"
+                }                 
+            }
+        )
+        file.seek(0)
+        file.truncate()
+        json.dump(file_data, file, indent=4 )
+        st.success(f"{stockLevel} of {item} on {floor}/F was updated on {time}")
--- pages/stockTake.py
+++ pages/stockTake.py
@@ -2,9 +2,12 @@ import streamlit as st
 import logic
 import os
 import json
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
-items = []
+# items = []
 
+# login requirement
 if not st.session_state.get("logged_in", False):
     st.error("Please login first!")
     st.switch_page("main.py")
@@ -12,35 +15,42 @@ if not st.session_state.get("logged_in", False):
 
 st.title("Stock Take Page")
 
+# intialize json data
 if not os.path.exists("stock.json"):
     intial_data = logic.initialize_json()
     with open("stock.json", "w") as f:
         json.dump(intial_data, f, indent=4)
 
-# User able to add new item by button "Add"
-new_item = st.text_input("Add Item")
+# User able to add or remove item in data by button "Add" and "Remove"
+new_item = st.text_input("Add Or Remove Item")
 col5, col6, col7 = st.columns([1, 1, 5])
 with col5:
     add = st.button("Add", use_container_width=True)
 with col6:
     remove = st.button("Remove", use_container_width=True)
 if add:
-    logic.add_item(new_item, items)
+    logic.add_item(new_item)
+if remove:
+    logic.remove_item(new_item)
 
+# read all the item in json file
 with open("stock.json", "r") as f:
     d = json.load(f)
     items = list(d["7"]["inventory"].keys())
 
+# update session
 st.subheader("Update")
 col1, col2, col3, col4 = st.columns([1, 4, 2, 1])
 with col1:
-    st.selectbox("Floor", options= logic.floors)
+    floor = st.selectbox("Floor", options= logic.floors)
 with col2:
-    st.selectbox("Stock", options=items)
+    item = st.selectbox("Stock", options=items)
 with col3:        
-    st.selectbox("Quantity in %", options= logic.quantity)
+    stockLevel = st.selectbox("Stock Level in %", options= logic.quantity)
 with col4:
     st.markdown("")
     st.markdown("")
-    st.button("Update")
-
+    update = st.button("Update")
+if update:
+    currTime = datetime.now(ZoneInfo("America/Halifax")).strftime("%D, %H:%M:%S")
+    logic.update_stock(floor, item, stockLevel, currTime)
